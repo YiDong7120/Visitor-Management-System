a335cd9f883694e49644c35a087b9dfaceae9374
Gan Aik Tong <andrewganaiktong@gmail.com>
2022-06-12 20:09:30 +0800

    Melancholia

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const options = {
 	definition: {
 		openapi: '3.0.0',
 		info: {
-			title: 'MyVMS API',
+			title: 'Welcome to Gan Hospital Visitor Management System',
 			version: '1.0.0',
 		},
 	},
@@ -36,7 +36,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-	res.send('Welcome to Hospital Visitor Management System')
+	res.send('Welcome to Gan Hospital Visitor Management System')
 })
 
 app.get('/hello', (req, res) => {
@@ -99,20 +99,6 @@ app.post('/login', async (req, res) => {
  *           type: string
  */
 
-/**
- * @swagger
- * /visitor,{id}:
- *   get:
- *     description: Get visitor information
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: Visitor ID
- */
-
 app.post('/register', async (req, res) => {
 	console.log(req.body);
 
@@ -126,6 +112,46 @@ app.post('/register', async (req, res) => {
 
 })
 
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     description: User Register
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema: 
+ *             type: object
+ *             properties:
+ *               username: 
+ *                 type: string
+ *               password: 
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Register success!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       401:
+ *         description: User already exits!
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id: 
+ *           type: string
+ *         username: 
+ *           type: string
+ */
+
 app.patch('/update', async (req, res) => {
 	console.log(req.body);
 	
@@ -138,11 +164,103 @@ app.patch('/update', async (req, res) => {
 	res.status(200).send("Update success!");
 })
 
+/**
+ * @swagger
+ * /update:
+ *   patch:
+ *     description: User Update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema: 
+ *             type: object
+ *             properties:
+ *               username: 
+ *                 type: string
+ *               newusername: 
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Update success!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       401:
+ *         description: Invalid username
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id: 
+ *           type: string
+ *         username: 
+ *           type: string
+ */
+
 app.delete('/delete', async (req, res) => {
 	console.log(req.body);
 
 	await User.delete(req.body.username);
 	return res.status(200).send("Delete success!");
+})
+
+/**
+ * @swagger
+ * /delete:
+ *   delete:
+ *     description: User Delete
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema: 
+ *             type: object
+ *             properties:
+ *               username: 
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Delete success!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id: 
+ *           type: string
+ *         username: 
+ *           type: string
+ */
+
+app.delete('/deleteVisitor', async (req, res) => {
+	console.log(req.body);
+
+	await User.deleteVisitor(req.params.visitor_id);
+	res.status(200).send("Delete success!");
+
+})
+
+app.delete('/deleteReservation', async (req, res) => {
+	console.log(req.body);
+
+	await User.deleteReservation(req.params.reservation_id);
+	res.status(200).send("Delete success!");
+
 })
 /////////////////////////////////////////////////
 
@@ -193,10 +311,50 @@ app.delete('/delete', async (req, res) => {
 	return res.status(200).send("Delete success!");
 })
 
-app.get('visitor/:id', async (req, res) => {
-	console.log(req.params.id);
-	res.status(200).json({})
-	})
+//Open to all
+app.get('/visitor/:id', async (req, res) => {
+	console.log(req.body);
+
+	let visitor = await User.getVisitor(req.params.visitor_id);
+	res.status(200).json(visitor)
+
+})
+
+/**
+ * @swagger
+ * /visitor,{id}:
+ *   get:
+ *     description: Get visitor information
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Visitor ID
+ */
+
+app.get('/reservation/:id', async (req, res) => {
+	console.log(req.body);
+
+	let reservation = await User.getReservation(req.params.reservation_id);
+	res.status(200).json(reservation)
+
+})
+
+/**
+ * @swagger
+ * /reservation,{id}:
+ *   get:
+ *     description: Get reservation information
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Reservation ID
+ */
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
--- main.test.js
+++ main.test.js
@@ -2,12 +2,12 @@ const supertest = require('supertest');
 const request = supertest('http://localhost:3000');
 
 describe('Express Route Test', function () {
-	it('should return hello world', async () => {
+	it('should return Hello from BENR2423', async () => {
 		return request.get('/hello')
 			.expect(200)
 			.expect('Content-Type', /text/)
 			.then(res => {
-				expect(res.text).toBe('Hello BENR2423');
+				expect(res.text).toBe('Hello from BENR2423');
 			});
 	})
 
@@ -87,6 +87,26 @@ describe('Express Route Test', function () {
 			});
 	})
 
+	it('delete visitor', async () => {
+		return request
+			.delete('/deleteVisitor')
+			.send({ visitor_id: "f4f5a27c-7d3d-40b5-b508-35debc009d84" })
+			.expect('Content-Type', /text/)
+			.expect(200).then(response => {
+				expect(response.text).toEqual("Delete success!");
+			});
+	});
+
+	it('delete reservation', async () => {
+		return request
+			.delete('/deleteReservation')
+			.send({ reserve_id: "25112e65-64d1-4a0c-9f87-6951d1bf4ea4" })
+			.expect('Content-Type', /text/)
+			.expect(200).then(response => {
+				expect(response.text).toEqual("Delete success!");
+			});
+	});
+
 /////////////////////////////////////////////
 	it('login successfully', async () => {
 		return request
@@ -163,4 +183,34 @@ describe('Express Route Test', function () {
 				expect(response.text).toEqual("Delete success!");
 			});
 	})
+
+	it('find visitor', async () => {
+		return request
+			.get('/visitor/7ac1937b-73aa-45d9-b89f-7ae962e1302f')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.expect(200).then(response => {
+				expect(response.body).toEqual(
+					expect.objectContaining({
+						_id: expect.any(String),
+						username: expect.any(String),
+					})
+				);
+			});
+	});
+
+	it('find reservation', async () => {
+		return request
+			.get('/reservation/0b8ded40-3495-4d02-8177-1265cc002f61')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.expect(200).then(response => {
+				expect(response.body).toEqual(
+					expect.objectContaining({
+						_id: expect.any(String),
+						username: expect.any(String),
+					})
+				);
+			});
+	});
 });
